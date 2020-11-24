5718ad3005e2f12be6011599c5afc4bd2746f955
TeodorP01 <tedi2001bg@abv.bg>
2020-11-24 15:36:53 +0000

    Attempt_1

--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -2,6 +2,24 @@ package exercise9;
 
 public class EllipseDemo {
     public static void main(String[] args) {
+      Ellipse ellipse1 = new Ellipse();
+      
+      Point p2 = new Point(2,3);
+      Ellipse ellipse2 = new Ellipse (p2, 5, 6);
 
+      Ellipse ellipse3 = new Ellipse(ellipse2);
+
+      Ellipse ellipse4 = new Ellipse();
+      ellipse4.initialize();
+
+      System.out.println("Ellipse1" + ellipse1);
+      System.out.println("Ellipse2" + ellipse2);
+      System.out.println("Ellipse3" + ellipse3);
+      System.out.println("Ellipse3" + ellipse4);
+
+      System.out.println("Ellipse 2 equals Ellipse 1" + ellipse2.equals(ellipse1));
+      System.out.println("Ellipse 2 equals Ellipse 3" + ellipse2.equals(ellipse3));
+
+        
     }
 }
